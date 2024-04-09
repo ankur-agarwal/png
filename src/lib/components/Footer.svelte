987c620ee527de86d965e96fb6cee5666e74724b
Ankur Agarwal <agarwalankur58@gmail.com>
2024-04-09 15:09:58 +0530

    setup footer skeleton

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,32 @@
+<footer class="w-full border-t h-24 flex items-center">
+  <div class="flex justify-between w-full">
+    <div>
+      <button type="button" class="w-32  justify-center inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+        Next
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+        </svg>
+
+      </button>
+    </div>
+
+    <div class="flex flex-row justify-center space-x-2">
+      <button type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-sky-500 border border-sky-500 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+        Preview
+      </button>
+
+      <button type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-sky-500 border border-sky-500 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>
+        Save Draft
+      </button>
+
+      <button type="button" class="w-32 justify-center inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
+        Finish
+      </button>
+    </div>
+  </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
   import Stepper from "$lib/components/Stepper/index.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+
+  let currentStep = 1;
+
 </script>
 
-<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 m-8">
-  <!-- Page Heading -->
-  <div class="min-w-0 flex-1">
-    <h2 class="flex justify-center text-4xl font-semibold leading-7 text-sky-700 sm:truncate sm:text-4xl sm:tracking-tight">
-      Build your Privacy Notice
-    </h2>
-  </div>
+<div class="flex justify-center h-screen">
+  <div class="flex flex-col h-full">
+    <!-- Page Heading -->
+    <div class="min-w-0 flex-1 max-w-7xl mt-8">
+      <h2 class="flex justify-center text-4xl font-semibold leading-7 text-gray-700 sm:truncate sm:text-4xl sm:tracking-tight">
+        Build your Privacy Notice
+      </h2>
 
-  <main class="mt-8">
-    <Stepper />
-  </main>
+      <div class="mt-8">
+        <Stepper />
+      </div>
+    </div>
+
+    <main class="flex-1 max-w-7xl">
+      <div></div>
+    </main>
+
+    <Footer />
+  </div>
 </div>
